ef45a8e57356bb5c350246f91297934953aa66dd
petschoice-owen <owen.caisip@petschoice.co.uk>
2023-08-31 23:09:03 +0800

    Restricted Shipping Zones WP - custom code

--- footer.php
+++ footer.php
@@ -76,5 +76,114 @@
         </div>
     </div>
     <?php wp_footer(); ?>
+
+    <!-- Cart Page -->
+    <?php if ( is_page( 'cart' ) || is_cart() ) { ?>
+        <script>
+            var $ = jQuery;
+
+            // add the restricted shipping zone message
+            $(document).ready(function() {
+                if ( $('.cart_totals').length ) {
+                    $('<div class="restricted-message-wrapper"><p class="restricted-message">Apologies, the provided address is currently not within our delivery range. We only accept orders within the UK mainland - for orders outside this region, please contact the sales office.</p></div>').insertAfter('.cart_totals');
+                }
+            });
+
+            $(document).ready(function() {
+                // validate shipping zone on page load
+                var shippingMethod = $('#shipping_method').text().trim();
+                if ( shippingMethod.includes("Restricted Zone") ) {
+                    // console.log("restricted zone");
+                    $('body').addClass('restricted-zone');
+                }
+                else {
+                    // console.log("UK Mainland");
+                    $('body').removeClass('restricted-zone');
+                }
+
+                // hide/show proceed to checkout button
+                if ( $('.woocommerce-shipping-methods').length ) {
+                    $('.wc-proceed-to-checkout').show();
+                }
+                else {
+                    $('.wc-proceed-to-checkout').hide();
+                }
+            });
+
+            $('body').on('updated_cart_totals', function() {
+                // console.log('updated_cart_totals triggered');
+                
+                // validate shipping zone on change address
+                var shippingMethod = $('#shipping_method').text().trim();
+
+                if ( shippingMethod.includes("Restricted Zone") ) {
+                    // console.log("restricted zone");
+                    $('body').addClass('restricted-zone');
+                }
+                else {
+                    // console.log("UK Mainland");
+                    $('body').removeClass('restricted-zone');
+                }
+
+                // hide/show proceed to checkout button
+                if ( $('.woocommerce-shipping-methods').length ) {
+                    $('.wc-proceed-to-checkout').show();
+                }
+                else {
+                    $('.wc-proceed-to-checkout').hide();
+                }
+            });
+        </script>
+    <?php } ?>
+
+    <!-- Checkout Page -->
+    <?php if ( is_page( 'checkout' ) || is_checkout() ) { ?>
+        <script>
+            var $ = jQuery;
+            // console.log("checkout - test");
+
+            // add the restricted shipping zone message
+            $(document).ready(function() {
+                if ( $('.woocommerce-checkout-review-order').length ) {
+                    $('<div class="restricted-message-wrapper"><p class="restricted-message">Apologies, the provided address is currently not within our delivery range. We only accept orders within the UK mainland - for orders outside this region, please contact the sales office.</p></div>').insertAfter('.woocommerce-checkout-review-order');
+                }
+            });
+
+            // validate shipping zone on page load
+            $(document).ready(function() {
+                var shippingMethod = $('#shipping_method').text().trim();
+                if ( shippingMethod.includes("Restricted Zone") ) {
+                    // console.log("on load - restricted zone");
+                    $('body').addClass('restricted-zone');
+                }
+                else {
+                    // console.log("on load - UK Mainland");
+                    $('body').removeClass('restricted-zone');
+                }
+            });
+
+            // validate shipping zone on change address
+            $('body').on('updated_checkout', function() {
+                // console.log('updated_checkout triggered');
+
+                var shippingMethod = $('#shipping_method').text().trim();
+
+                if ( shippingMethod.includes("Restricted Zone") ) {
+                    // console.log("restricted zone");
+                    $('body').addClass('restricted-zone');
+                }
+                else {
+                    // console.log("UK Mainland");
+                    $('body').removeClass('restricted-zone');
+                }
+            });
+
+            // validate shipping zone on change postcode
+            $('#billing_postcode, #shipping_postcode').on('focusout, blur', function() {
+                // console.log('updated_checkout triggered');
+                $(document.body).trigger('update_checkout');
+            });
+        </script>
+    <?php } ?>
 </body>
 </html>
